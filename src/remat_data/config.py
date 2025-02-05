01a61c2b8481d59ed3be348427fc342b4c9f32f0
at71 <at71@illinois.edu>
2025-02-05 15:30:46 -0600

    Move mapping to config.py

---
+++ src/remat_data/config.py
@@ -0,0 +1,13 @@
+config = {
+    "default_new_dataset_name": "Default Dataset",
+    "clowder_base_url": "https://re-mat.clowder.ncsa.illinois.edu",
+    "dataset_path": "datasets",
+}
+
+# Space name to UUID mapping. Add new spaces here
+space_map = {
+    "DSC Cure Kinetics": "64343b6be4b01a23c58bad90",
+    "DSC Post Cures": "6669d4d0e4b0a2d1b9b9a797",
+    "Front velocities": "6674972be4b0a2d1b9ba0228",
+}
+
