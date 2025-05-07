a085ab432e04aa2b7a168b1727069034ec324c41
at71 <at71@illinois.edu>
2025-05-07 16:34:49 -0500

    change cure_kin (id)

--- src/remat_data/config.py
+++ src/remat_data/config.py
@@ -8,7 +8,8 @@ config = {
 
 # Space name to UUID mapping. Add new spaces here
 space_map = {
-    "DSC Cure Kinetics": "64343b6be4b01a23c58bad90",
+    "DSC Cure Kinetics": "6810f088e4b00420021cff64",
     "DSC Post Cures": "6669d4d0e4b0a2d1b9b9a797",
     "Front velocities": "6674972be4b0a2d1b9ba0228",
+    "Test": "67edd4e8e4b00fd657cdd863",
 }
